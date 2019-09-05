eadc399208eae757e8e44d68f945b61d5169a9ca
Issac Wong (stw) <stw09701125@gmail.com>
2019-09-05 17:42:31 +0800

    Arguments Query test OK

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const { ApolloServer, gql } = require('apollo-server');
 
 // Mock data
 const users = [
-  { id: 1, name: 'Rebecca', age: 23, friendsIds: [2, 3] },
-  { id: 2, name: 'Sandy', age: 25, friendsIds: [1] },
-  { id: 3, name: 'Nathalie', age: 23, friendsIds: [1] },
+  { id: 1, name: 'Rebecca', age: 23, friendsIds: [2, 3], height: 160, weight: 45 },
+  { id: 2, name: 'Sandy', age: 25, friendsIds: [1], height: 150, weight: 43 },
+  { id: 3, name: 'Nathalie', age: 23, friendsIds: [1], height: 162, weight: 50 },
 ];
 
 
@@ -21,6 +21,20 @@ const typeDefs = gql`
     name: String
     age: Int
     friends: [User]
+    height(unit: HeightUnit = CENTIMETER): Float
+    weight(unit: WeightUnit = KILOGRAM): Float
+  }
+
+  enum HeightUnit {
+    METER
+    CENTIMETER
+    FOOT
+  }
+
+  enum WeightUnit {
+    KILOGRAM
+    GRAM
+    POUND
   }
 `;
 
@@ -37,6 +51,20 @@ const resolvers = {
     friends: (parent, args, context) => {
       const { friendsIds } = parent;
       return users.filter(user => friendsIds.includes(user.id));
+    },
+    height: (parent, args, context) => {
+      const { unit } = args;
+      if (!unit || unit === 'CENTIMETER') return parent.height;
+      else if (unit === 'METER') return parent.height / 100;
+      else if (unit === 'FOOT') return parent.height / 30.48;
+      throw new Error(`Height unit "${unit}" not supported.`);
+    },
+    weight: (parent, args, context) => {
+      const { unit } = args;
+      if (!unit || unit === 'KILOGRAM') return parent.weight;
+      else if (unit === 'GRAM') return parent.weight * 1000;
+      else if (unit === 'POUND') return parent.weight / 0.45359237;
+      throw new Error('Weight unit "${unit}" not supported.');
     }
   }
 };
