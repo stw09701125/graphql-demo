66df9e9864ad4b78f53a389654d05367e8a91cc8
Issac Wong (stw) <stw09701125@gmail.com>
2019-09-05 17:22:59 +0800

    add mock data, User type to schema and friends Field Resolver of User

--- index.js
+++ index.js
@@ -1,15 +1,43 @@
 const { ApolloServer, gql } = require('apollo-server');
 
+// Mock data
+const users = [
+  { id: 1, name: 'Rebecca', age: 23, friendsIds: [2, 3] },
+  { id: 2, name: 'Sandy', age: 25, friendsIds: [1] },
+  { id: 3, name: 'Nathalie', age: 23, friendsIds: [1] },
+];
+
+
 const typeDefs = gql`
   type Query {
     "A simple type for getting started!"
     hello: String
+    me: User
+    users: [User]
+  }
+
+  type User {
+    id: ID!
+    name: String
+    age: Int
+    friends: [User]
   }
 `;
 
 const resolvers = {
   Query: {
-    hello: () => 'world'
+    hello: () => 'world',
+    me: () => users[0],
+    users: () => users,
+  },
+  User: {
+    id: (parent) => parent.id,
+    name: (parent) => parent.name,
+    age: (parent) => parent.age,
+    friends: (parent, args, context) => {
+      const { friendsIds } = parent;
+      return users.filter(user => friendsIds.includes(user.id));
+    }
   }
 };
 
