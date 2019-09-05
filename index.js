e40a9fc64965f36285ebf05b1953fb913d61318e
Issac Wong (stw) <stw09701125@gmail.com>
2019-09-05 14:43:59 +0800

    :sparkles: init

---
+++ index.js
@@ -0,0 +1,20 @@
+const { ApolloServer, gql } = require('apollo-server');
+
+const typeDefs = gql`
+  type Query {
+    "A simple type for getting started!"
+    hello: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => 'world'
+  }
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.listen().then(({ url }) => {
+  console.log(`? Server ready at ${url}`);
+});
